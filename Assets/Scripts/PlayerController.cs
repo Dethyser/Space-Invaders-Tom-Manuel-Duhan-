8b8bbdc39bd27f9fd9b3600b2ad0a5e995b02920
Dethyser <71837325+Dethyser@users.noreply.github.com>
2021-01-12 18:22:32 +0100

    vers 0.1
    -added PlayerMovement
    -added ShootingAbility

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float input;
+    public float movementSpeed = 5f;
+
+    public string axisName;
+
+    private Rigidbody2D rb;
+
+    public GameObject shotPrefab;
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        
+            input = Input.GetAxisRaw(axisName);
+        if (Input.GetButtonDown("Jump")) {
+
+            SpawnShot();
+        }
+    }
+
+    private void FixedUpdate() {
+
+        rb.velocity = Vector2.right * input * movementSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+    
+    }
+
+    private void SpawnShot() {
+
+        Instantiate(shotPrefab, transform);
+    }
+}
---
+++ Assets/Scripts/ShotController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShotController : MonoBehaviour {
+
+    public float movementSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+
+        rb.velocity = Vector2.up * movementSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+    }
+}
